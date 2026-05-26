9624b7eca15ff5661948c10fa940d0b345b0410f
rinielezaj <rinielezaj08@gmail.com>
2026-05-26 20:23:27 +0200

    lesson 4-5

--- functions.php
+++ functions.php
@@ -8,12 +8,30 @@
 // }
 // add_action('wp_enqueue_scripts','add_theme_scripts');
 
-function ds_setup(){
-    add_theme_support('menus');
-    register_nav_menu('primary', "Primary Navigation");
-}
+// function ds_setup(){
+//     add_theme_support('menus');
+//     register_nav_menu('primary', "Primary Navigation");
+// }
 
-add_action("init","ds_setup");
+// add_action("init","ds_setup");
 
-wp_nav_menu(array('theme_location'=>'primary'));
+// wp_nav_menu(array('theme_location'=>'primary'));
+?>
+
+<?php
+    add_theme_support('post-thumbnails');
+
+    function defino_bs_styles(){
+        wp_enqueue_style("bootstrap", get_template_directory_uri().'./css/bootstrap.min.css');
+        wp_enqueue_style("core",get_template_directory_uri().'./style.css');
+    }
+
+    add_action('wp_enqueue_scripts','defino_bs_styles');
+
+    function themebs_scripts(){
+        wp_enqueue_scripts('bootstrap', get_template_directory_uri().'.js/bootstrap.min.js',
+        array("jquery"));
+    }
+    
+    add_action('wp_enqueue_scripts','themebs_scripts');
 ?>
